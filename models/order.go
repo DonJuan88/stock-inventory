4487d13794f2df2c0044be81ab9e607773f6d1a0
DonJuan88 <hanamichi_dony@yahoo.co.id>
2024-12-16 22:57:25 +0700

    Adding status in purchasing=sales table

--- models/order.go
+++ models/order.go
@@ -8,15 +8,15 @@ import (
 
 type Order struct {
 	gorm.Model
-	OrderNo      string    `json:"order_no"`
-	OrderDate    time.Time `json:"order_date"`
-	ShippingCost int64     `json:"shippingprice"`
-	Tax1         int64     `json:"tax1"`
-	Tax2         int64     `json:"tax2"`
-	Total        int64     `json:"total"`
-	AccountID    string    `json:"accid"`
-	PaymentType  string    `json:"paymenttype"`
-	Reference    string    `json:"reference"`
-	Notes        string    `json:"notes"`
-	Lunas        bool      `jaon:"status gorm:default:false"`
+	OrderNo       string    `json:"order_no"`
+	OrderDate     time.Time `json:"order_date"`
+	ShippingCost  int64     `json:"shippingprice"`
+	Tax1          int64     `json:"tax1"`
+	Tax2          int64     `json:"tax2"`
+	Total         int64     `json:"total"`
+	AccountID     string    `json:"accid"`
+	PaymentType   string    `json:"paymenttype"`
+	Reference     string    `json:"reference"`
+	Notes         string    `json:"notes"`
+	PaymentStatus bool      `jaon:"status gorm:default:false"`
 }
